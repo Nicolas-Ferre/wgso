43546e8cbc81d7572bfbbcdaa35a9b2caedc89a9
Nicolas Ferré <Nicolas-Ferre@users.noreply.github.com>
2025-04-20 21:27:16 +0200

    Add main surface (#39)

--- crates/wgso/tests/integration/main.rs
+++ crates/wgso/tests/integration/main.rs
@@ -1,5 +1,6 @@
 #![allow(missing_docs, clippy::unwrap_used)]
 
+mod draw;
 mod invalid_cases;
 mod runner;
 mod valid_cases;
--- crates/wgso/tests/integration/runner.rs
+++ crates/wgso/tests/integration/runner.rs
@@ -2,7 +2,8 @@ use wgso::{Error, Runner};
 
 #[test]
 fn run_invalid_directory_path() {
-    let program = Runner::new("invalid_path").expect_err("invalid path has not returned error");
+    let program =
+        Runner::new("invalid_path", None, None).expect_err("invalid path has not returned error");
     assert_eq!(program.errors.len(), 1);
     assert!(matches!(program.errors[0], Error::Io(_, _)));
     assert!(program.errors[0]
@@ -12,26 +13,26 @@ fn run_invalid_directory_path() {
 
 #[test]
 fn retrieve_not_existing_buffer() {
-    let runner = Runner::new("tests/cases_valid/storages").unwrap();
+    let runner = Runner::new("tests/cases_valid/storages", None, None).unwrap();
     assert_eq!(runner.read("invalid"), vec![]);
 }
 
 #[test]
 fn read_valid_buffer_field() {
-    let mut runner = Runner::new("tests/cases_valid/uniforms").unwrap();
+    let mut runner = Runner::new("tests/cases_valid/uniforms", None, None).unwrap();
     runner.run_step().unwrap();
     assert_eq!(runner.read("modes.inner.mode1"), vec![1, 0, 0, 0]);
 }
 
 #[test]
 fn read_invalid_buffer_field() {
-    let runner = Runner::new("tests/cases_valid/uniforms").unwrap();
+    let runner = Runner::new("tests/cases_valid/uniforms", None, None).unwrap();
     assert_eq!(runner.read("modes.invalid"), vec![]);
 }
 
 #[test]
 fn run_init_shaders_only_once() {
-    let mut runner = Runner::new("tests/cases_valid/shader_run_ordering").unwrap();
+    let mut runner = Runner::new("tests/cases_valid/shader_run_ordering", None, None).unwrap();
     runner.run_step().unwrap();
     runner.run_step().unwrap();
     assert_eq!(runner.read("buffer"), vec![65, 0, 0, 0]);
