614edcfc345ad1ff65765bacc0cc9afe9678a5d1
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2025-05-10 14:28:47 +0200

    Fix tests

--- crates/wgso/tests/integration/examples.rs
+++ crates/wgso/tests/integration/examples.rs
@@ -3,7 +3,7 @@ use wgso::Runner;
 
 #[rstest::rstest]
 fn run_valid_code(#[files("../../examples/*")] path: PathBuf) {
-    wgso_deps::retrieve_dependencies(&path).unwrap();
+    wgso_deps::retrieve_dependencies(path.join("wgso.yaml")).unwrap();
     let mut runner = Runner::new(&path, None, Some((10, 8))).unwrap();
     runner.run_step().unwrap();
 }
--- examples/3d/constants.wgsl
+++ examples/3d/constants.wgsl
@@ -1,3 +1,1 @@
 const RATIO = 800. / 600.;
-
-// TODO: try to optimize transformations and quaternion operations
