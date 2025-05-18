0247a0e5a18fef4a9361f850cbb0a6581a868f38
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2025-05-18 16:07:31 +0200

    Fix lints

--- crates/wgso/examples/main_android.rs
+++ crates/wgso/examples/main_android.rs
@@ -6,7 +6,7 @@
 #[cfg(target_os = "android")]
 #[allow(unsafe_code)]
 #[no_mangle]
-pub extern "C" fn android_main(app: android_activity::AndroidApp) {
+pub extern "Rust" fn android_main(app: android_activity::AndroidApp) {
     static PROJECT_DIR: include_dir::Dir<'_> = include_dir::include_dir!("$PROGRAM_PATH");
     let program_path = app
         .internal_data_path()
@@ -26,3 +26,4 @@ pub extern "C" fn android_main(app: android_activity::AndroidApp) {
     };
     args.run_android(app);
 }
+    
